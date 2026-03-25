7e2cf60fc13c78a97f9a1f40349b782b1e26893c
MichaelAguirre27 <michaelaguirre@my.unt.edu>
2026-03-25 15:23:45 -0500

    Create sign_in.js

---
+++ sign_in.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website loaded successfully!");
+
+
+    alert("Welcome to my Bootstrap Website!");
+});
+
+
+function showMessage() {
+    alert("Thanks for clicking! - Michael Aguirre");
+}
+
+
+function changeText() {
+    let textElement = document.getElementById("dynamicText");
+    textElement.innerHTML = "You just changed this text using JavaScript!";
+}
+
+
+function toggleDarkMode() {
+    document.body.classList.toggle("bg-dark");
+    document.body.classList.toggle("text-white");
+}
